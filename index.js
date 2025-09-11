3e2b423e2dbdb631cdf5d444d9da6f4f72f247e4
BrunoClemes <Brunojoseclemes@hotmail.com>
2025-09-11 16:27:25 -0300

    mudanças

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-{
-    function tocarAudio(id) {
-      const audio = document.getElementById(id);
-      if (audio) {
-        audio.play()
-      }
-    }
+// Função para tocar o áudio
+function tocarAudio(id) {
+  const audio = document.getElementById(id);
+  if (audio) {
+    audio.play();
   }
+}
+
+// Seletores do modal
+const modal = document.getElementById("modal");
+const abrirMoldal = document.getElementById("abrirMoldal");
+const closeModal = document.getElementById("closeModal");
+
+// Abre o modal
+abrirMoldal.addEventListener("click", () => {
+  modal.showModal();
+});
+
+// Fecha o modal
+closeModal.addEventListener("click", () => {
+  modal.close();
+});
+
